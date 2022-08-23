75036c4ff4c509a97f9275269eba4064b81e3d21
Ruben Hernandez <rubhernandez42@gmail.com>
2022-08-23 15:36:06 -0400

    Se agrego el .gitignore y lo demas archivos del proyecto

---
+++ writing.js
@@ -0,0 +1,36 @@
+// ESCRIBIENDO HTML DESDE JS
+
+// const p = document.querySelectorAll("p");
+
+// const parrafito = document.getElementsByClassName("parrafito");
+
+// const pid = document.getElementById("pid");
+
+// const input = document.querySelector("input");
+
+// console.log({
+//   h1,
+//   p,
+//   parrafito,
+//   pid,
+//   input,
+// });
+
+// h1.innerHTML = "Cambiando el titulo"; ///convierte todo a codigo incluso si lo pones
+// h1.innerText = "Rescribiendo y aprendiendo sobre funciones"; /// protege mas tu codigo de ser editado solo siendo texto
+
+// console.log(h1.getAttribute("class")); /// lee atributos que tenga nuestros elementos
+// h1.setAttribute("class", "azul"); /// forma de modificar los atributos en html
+
+// h1.classList.add("negro");
+// h1.classList.remove("azul");
+
+// const img = document.createElement("img");
+// img.setAttribute(
+//   "src",
+//   "https://i.pinimg.com/originals/8b/02/40/8b02409f975c98c89e15bc089e3bd289.gif"
+// );
+// console.log(img);
+
+// pid.innerHTML = "";
+// pid.appendChild(img);
